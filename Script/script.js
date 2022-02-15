4b8fb441f5caf6c993ba13ecc5fc9085a5a72970
Vahram5 <vahram-khachikyan@mail.ru>
2022-02-15 22:43:40 +0400

    photos

--- Script/script.js
+++ Script/script.js
@@ -1,13 +1,37 @@
 $(function(){
-   
-
+    setInterval(() => {
+        let img_number;
+        let class_check = $(".full").attr("class");
+        if (class_check.indexOf("c1") >=0){
+            img_number = 1;
+        }
+        else if(class_check.indexOf("c2") >=0){
+            img_number = 2;
+        }
+        else if(class_check.indexOf("c3") >=0){
+            img_number = 3;
+        }
+        img_number++;
+        img_number == 4 ? img_number = 1 : img_number;
+        img_url = "url('./Img/"+img_number+".jpg') no-repeat center/cover";
+        $(".header").attr("id", img_number);
+        $(".header").css("background", img_url);
+        if (img_number - 1 == 0){
+            $(".c"+(3)).toggleClass("full");
+        }
+        else{
+            $(".c"+(img_number-1)).toggleClass("full");
+        }
+        $(".c"+img_number).toggleClass("full");
+    }, 5000);
 
     $(".left_arrow").click(function(){
-        let img_number = $(".header").attr("id");
+        img_number = $(".header").attr("id");
         $(".c"+img_number).toggleClass("full");
         img_number--;
         img_number==0 ? img_number=3 : img_number;
-        for(let i = 1; i <= 3; i++) {
+        for(let i = 1; i <= 3; i++)
+        {
             if(i==img_number){
                 img_url = "url('./Img/"+i+".jpg') no-repeat center/cover";
                 $(".header").attr("id", img_number);
@@ -18,7 +42,7 @@ $(function(){
     })
 
     $(".right_arrow").click(function(){
-        let img_number = $(".header").attr("id");
+        img_number = $(".header").attr("id");
         $(".c"+img_number).toggleClass("full");
         img_number++;
         img_number==4 ? img_number=1 : img_number;
