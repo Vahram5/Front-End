387ad7d111a92ac731b15dff36ae205e46b2debd
Vahram <vahram-khachikyan@mail.ru>
2022-05-12 20:36:18 +0400

    updated

--- Script/script.js
+++ Script/script.js
@@ -1,29 +1,4 @@
 $(function(){
-    setInterval(() => {
-        let img_number;
-        let class_check = $(".full").attr("class");
-        if (class_check.indexOf("c1") >=0){
-            img_number = 1;
-        }
-        else if(class_check.indexOf("c2") >=0){
-            img_number = 2;
-        }
-        else if(class_check.indexOf("c3") >=0){
-            img_number = 3;
-        }
-        img_number++;
-        img_number == 4 ? img_number = 1 : img_number;
-        img_url = "url('./Img/"+img_number+".jpg') no-repeat center/cover";
-        $(".header").attr("id", img_number);
-        $(".header").css("background", img_url);
-        if (img_number - 1 == 0){
-            $(".c"+(3)).toggleClass("full");
-        }
-        else{
-            $(".c"+(img_number-1)).toggleClass("full");
-        }
-        $(".c"+img_number).toggleClass("full");
-    }, 5000);
 
     $(".left_arrow").click(function(){
         img_number = $(".header").attr("id");
